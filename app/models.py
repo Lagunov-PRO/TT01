80ec0d06b5b164eb34e64bf6bbfcc022e188c602
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-24 18:50:51 +0300

    Подсчёт колличества серверов в проекте внутри БД

--- app/models.py
+++ app/models.py
@@ -1,19 +1,22 @@
-from sqlalchemy.orm import relationship
+from sqlalchemy import select, func
+from sqlalchemy.orm import relationship, column_property
 
 from . import db, mm
 
 
-class Task(db.Model):  # renamed from Tasks to Projects
+class User(db.Model):  # renamed from Tasks to Projects
     __tablename__ = 'projects'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), nullable=False)
-    servers = relationship("Servers")
+    servers = relationship('Server')
+    # Подсчёт колличества серверов в проекте
+    # servers_count = column_property(select([func.count(Server.id)]).where(Server.project_id == id))
 
-    def __init__(self, name):
-        self.name = name
-
-    def as_dict(self):
-        return {'id': self.id, 'name': self.name}
+    # def __init__(self, name):
+    #     self.name = name
+    #
+    # def as_dict(self):
+    #     return {'id': self.id, 'name': self.name}
 
 
 class Server(db.Model):
@@ -21,7 +24,6 @@ class Server(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), nullable=False)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
-
 
 
 class TaskSchema(mm.ModelSchema):
