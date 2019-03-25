7fa26c93b4b74787d287739c9e04b9ff9caae78c
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-25 22:26:22 +0300

    Removed CORS for this project

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
-# from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
 app.config.from_envvar('RUN_CFG')
-# CORS(app)
 db = SQLAlchemy(app)
 mm = Marshmallow(app)
 
