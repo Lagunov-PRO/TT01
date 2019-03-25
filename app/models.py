a9e67f9c365e00abfcdc56664bd61d7c30e1ddd7
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-25 07:36:16 +0300

    Translated comments to Russian

--- app/models.py
+++ app/models.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import relationship, column_property, validates
 from . import db, mm
 
 
-class User(db.Model):  # renamed from Tasks to Projects
+class Project(db.Model):  # renamed from Tasks to Projects
     __tablename__ = 'projects'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), nullable=False)
     servers = db.relationship('Server')
 
-    #  Checking that name has only letters
+    #  Проверяем, что имя содержит только буквы
     @validates('name')
     def validate_name(self, value):
         assert value.isalpha()
@@ -32,9 +32,16 @@ class Server(db.Model):
     name = db.Column(db.String(20), nullable=False)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
 
-    #  Checking that name has only letters
+    #  Проверяем, что добавляемый project_id встречается меньше двух раз
+    @validates('project_id')
+    def validate_projects_count(self, key, value):
+        projects_count = column_property(select([func.count(value)]))
+        assert projects_count <= 2
+        return value
+
+    #  Проверяем, что имя содержит только буквы
     @validates('name')
-    def validate_name(self, value):
+    def validate_name(self, key, value):
         assert value.isalpha()
         return value
 
