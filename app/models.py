b86a560b7df5d8b3d86b0af2f96446ddee59283d
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-26 02:21:56 +0300

    Validating that one project id is used no more than three times in servers table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from flask_marshmallow import Schema
+from marshmallow import ValidationError, fields, validates
+
 from . import db, mm
 
 
@@ -27,17 +30,25 @@ class Server(db.Model):
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False, unique=True)  # FIXME: required=True not working
 
     #  Проверяем, что добавляемый project_id встречается меньше двух раз
-    # @db.validates('project_id')
-    # def validate_projects_count(self, key, value):
-    #     projects_count = db.column_property(db.select([db.func.count(value)]))
-    #     assert projects_count <= 2
-    #     return value
+    @db.validates('project_id')
+    def validate_projects_count(self, key, value):
+        # projects_count = db.column_property(db.select([db.func.count(value)]))
+        projects_count = db.column_property(db.select([db.func.count(value)]))
+        assert projects_count <= 2
+        return value
+
+    @validates('project_id')
+    def check_project_has_3_servers(self, project_id):
+        projects_id_list = Server.query.filter_by(project_id=project_id).all()
+        if len(projects_id_list) == 3:
+            raise ValidationError('В одном проекте не больше трёх серверов.')
 
     #  Проверяем, что имя содержит только буквы
     @db.validates('name')
     def validate_name(self, key, value):
         assert value.isalpha()
         return value
+
 
     def __init__(self, name, project_id):
         self.name = name
@@ -47,14 +58,32 @@ class Server(db.Model):
         return {'id': self.id, 'name': self.name}
 
 
+#  Custom validators
+def check_server_without_project(data):
+    if not data:
+        raise ValidationError('Нельзя добавлять сервера без проектов.')
+
+
+#  SELECT * FROM server WHERE project_id = data
+# def check_project_has_3_servers(project_id):
+#     projects_id_list = Server.query.filter_by(project_id=project_id).all()
+#     if len(projects_id_list) == 3:
+#         raise ValidationError('В одном проекте не больше трёх серверов.')
+
+
 class ProjectSchema(mm.ModelSchema):
     class Meta:
         fields = ('id', 'name', 'servers')
 
 
-class ServerSchema(mm.ModelSchema):
-    class Meta:
-        fields = ('id', 'name', 'project_id')
+class ServerSchema(Schema):
+    id = fields.Int(dump_only=True)
+    project_id = fields.Nested(ProjectSchema, required=True, validate=check_server_without_project)
+
+
+# class ServerSchema(mm.ModelSchema):
+#     class Meta:
+#         fields = ('id', 'name', 'project_id')
 
 
 #  TODO: перенести верификацию в marshmallow
