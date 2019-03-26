6530f3514546c2ce939c445544c0c31aff882458
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-03-26 19:51:58 +0300

    Testing for response

--- app/test/test_db_crud.py
+++ app/test/test_db_crud.py
@@ -1,8 +1,12 @@
 import unittest
+from app import app
 
 
-def test_get_posts():
-    pass
+class ProjectsTestCase(unittest.TestCase):
+    def test_get_posts(self):
+        tester = app.test_client(self)
+        response = tester.get('/api/projects', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
